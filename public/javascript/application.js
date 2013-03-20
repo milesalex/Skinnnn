61b19985510e0f72f4b0c263086295d9b9bd1293
Alex Miles <howdy@alexmilesdesign.com>
2013-03-20 22:55:10 +0100

    Created makeEditable function in application.js

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -3,13 +3,27 @@
   var EditView = Backbone.View.extend({
     el: $('body'),
 
+    makeEditable: function(){
+      // Test if user is logged in
+      // There should be a safer way to do this
+      if ( $(this.el).hasClass('logged-in') ){
+        // Add contenteditable attr to elements
+        $('h1.name').attr( 'contenteditable', 'true' );
+        $('p.description').attr( 'contenteditable', 'true' );
+      } else {
+        // User is logged out
+        console.log('NOT logged in');
+      }
+    },
+
     initialize: function(){
       _.bindAll(this, 'render');
+      this.makeEditable();
       this.render();
     },
 
     render: function(){
-      $(this.el).prepend('works');
+      // $(this.el).prepend('works');
     }
   });
 
