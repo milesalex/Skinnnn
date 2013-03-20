e480a5b32d1dc342e0651a62187778d4fd12a6a5
Alex Miles <howdy@alexmilesdesign.com>
2013-03-20 22:20:52 +0100

    Set up backbone edit view

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,1 +1,18 @@
+(function($){
 
+  var EditView = Backbone.View.extend({
+    el: $('body'),
+
+    initialize: function(){
+      _.bindAll(this, 'render');
+      this.render();
+    },
+
+    render: function(){
+      $(this.el).prepend('works');
+    }
+  });
+
+  var editView = new EditView();
+
+})(jQuery);
