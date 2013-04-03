01c38755f6ea76b36e8292f2383bc962c5688c30
Martin Klepsch <martinklepsch@gmail.com>
2013-04-03 23:53:07 +0200

    adds carrierwave S3 initialization

--- app.rb
+++ app.rb
@@ -6,12 +6,24 @@ require "dm-core"
 require "dm-migrations"
 require "dm-serializer/to_json"
 require "dm-constraints"
+require "carrierwave/datamapper"
 require "route_downcaser"
 require "json"
 require "sinatra"
 require "slim"
 
 use RouteDowncaser::DowncaseRouteMiddleware
+
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',
+    :aws_access_key_id      => 'AKIAJ6YGSQUDIULAULNQ',
+    :aws_secret_access_key  => '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',
+    :region                 => 'eu-west-1',
+  }
+  config.fog_directory  = 'skinnnn'
+end
+
 
 require "./model"
 require "./helpers"
