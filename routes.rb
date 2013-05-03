ba227975763693251179555c73f13a60df73af07
Alex Miles <howdy@alexmilesdesign.com>
2013-05-03 11:20:42 +0200

    Use hash rockets instead of json style hashes

--- routes.rb
+++ routes.rb
@@ -12,21 +12,21 @@ before do
 end
 
 get '/' do
-  home = User.new(name: "Skinnnn", nickname: "home", email: "hi@skinnnn.com",
-                  bio: "Show who you are.", city: "Berlin")
+  home = User.new(:name => "Skinnnn", :nickname => "home", :email => "hi@skinnnn.com",
+                  :bio => "Show who you are.", :city => "Berlin")
 
   # home.profile   = Profile.new(bio: "Got Skinnnn?", city: "Berlin")
-  twitter_signup = Link.new(name: "Sign up with Twitter", url: "/sign_in")
-  manifest       = Link.new(name: "Our Manifest", url: "/manifest")
+  twitter_signup = Link.new(:name => "Sign up with Twitter", :url => "/sign_in")
+  manifest       = Link.new(:name => "Our Manifest", :url => "/manifest")
 
   home.links = [ twitter_signup, manifest ]
 
-  slim :profile, :locals => { user: home }
+  slim :profile, :locals => { :user => home }
 end
 
 get '/people/?' do
   users = User.all
-  slim :people, :locals => { users: users }
+  slim :people, :locals => { :users => users }
 end
 
 get '/randommmm/?' do
@@ -39,13 +39,13 @@ get '/:nickname/?' do
   user = User.first(:nickname => params[:nickname])
   halt 404 if user.nil?
   if user.links.empty?
-    twitter = Link.create(name: "Twitter", url: "http://twitter.com/mklappstuhl")
-    dribble = Link.create(name: "Dribble", url: "http://dribbble.com/gabrielvaldivia")
-    google = Link.create(name: "Google+", url: "https://plus.google.com/u/0/103892258076354730962")
+    twitter = Link.create(:name => "Twitter", :url => "http://twitter.com/mklappstuhl")
+    dribble = Link.create(:name => "Dribble", :url => "http://dribbble.com/gabrielvaldivia")
+    google = Link.create(:name => "Google+", :url => "https://plus.google.com/u/0/103892258076354730962")
     user.links = [ twitter, dribble, google ]
   end
   puts '@user'
-  slim :profile, :locals => { user: user }
+  slim :profile, :locals => { :user => user }
 end
 
 not_found do
